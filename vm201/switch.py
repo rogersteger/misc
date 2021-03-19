e9bd27ccb15114da150a1b5689a362dd91848c15
Roger Steger <roger.steger@gmail.com>
2021-03-19 09:37:34 +0100

    add first test script

---
+++ vm201/switch.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import os, sys
+import time
+from VM201RelayCard import *
+
+
+#------------------------------------------------------------------------------
+def main():
+    print("----------------------------------------------------------------")
+    print("Switch.py script")
+    print("----------------------------------------------------------------")
+    
+    
+    sw = VM201RelayCard('10.62.33.22')
+    sw.connect()
+    sw.status()
+    
+    sw.on_off_toggle('CMD_ON', 7)
+    time.sleep(10)
+    sw.on_off_toggle('CMD_OFF', 7)
+    
+#------------------------------------------------------------------------------
+if __name__ == "__main__":
+    exit(main())
